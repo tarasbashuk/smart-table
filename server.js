a31ed6dafb4deabebb0dcccb661eb015f08dcee8
Taras Bashuk <bashuk.taras@gmail.com>
2019-11-12 10:35:54 +0200

    add server for heroku

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const path = require('path')
+
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+
+
+app.use(express.static(__dirname))
+app.use(express.static(path.join(__dirname, 'dist')))
+
+app.get('/test', function (req, res) {
+ return res.send('Server running');
+})
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+})
+
+app.listen(PORT, () => console.log(`Server started on ${PORT}`))
